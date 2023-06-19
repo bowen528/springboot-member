38fef77debc427cbe41cbc94283d4ef258233404
bowen528 <hhilary5432@gmail.com>
2023-06-20 05:12:55 +0800

    初始化 Springboot

---
+++ src/main/java/com/bowen/springbootmember/SpringbootMemberApplication.java
@@ -0,0 +1,13 @@
+package com.bowen.springbootmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bowen/springbootmember/SpringbootMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowen.springbootmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
